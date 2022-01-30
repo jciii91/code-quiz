f5cac1544ced9781a129b4a5f08ba473c61dabef
JCIII <jcostello711@gmail.com>
2022-01-29 23:00:12 -0500

    high scores display. clearing and viewing of scores works as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,10 @@ function loadQuestion(quizQuestion) {
 }
 
 function Quiz() {
+    timeLeft = 75;
+    quizArray = fisherYatesShuffler(quizQuestions);
+    counter = quizArray.length-1;
+    score = 0;
     countdown();
     loadQuestion(quizArray[counter]);
 }
@@ -47,7 +51,7 @@ function itIsOver() {
     if (timeLeft < 0) {
         timeLeft = 0;
     }
-    score += timeLeft; 
+    score += timeLeft;
     $("main").empty();
     $("main").append("<h2 id=\"header-row\" class=\"row col-lg-12 d-flex justify-content-center\">All done!</h2>");
     $("main").append("<p class=\"row col-lg-12 d-flex justify-content-center\">Your final score is " + score + "</p>");
@@ -56,11 +60,42 @@ function itIsOver() {
     $("#score-submission").append("<p class=\"col-lg-5 d-flex justify-content-end\"><span>Please enter your name: </span></p><input type=\"text\" id=\"username\" class=\"form-control col-lg-5\"><button id=\"submit-button\" class=\"btn m-1\">Submit</button></div>");
 }
 
+function showScores() {
+    $("main").empty();
+    $("main").append("<div id=\"header-row\" class=\"row\"></div>");
+    $("#header-row").append("<div id=\"header-row-columns\" class=\"col-lg-12 d-flex justify-content-center\"></div>");
+    $("#header-row-columns").append("<h2>High Scores</h2>");
+
+    $("main").append("<div id=\"scores-container\" class=\"row d-flex justify-content-center\"></div>");
+    $("#scores-container").append("<div id=\"scores-table\" class=\"w-25 d-flex justify-content-center\"></div>");
+    $("#scores-table").append("<div id=\"names-column\" class=\"col-lg-6 d-flex justify-content-start\"></div>");
+    $("#names-column").append("<p>Names</p>");
+    $("#scores-table").append("<div id=\"scores-column\" class=\"col-lg-6 d-flex justify-content-end\"></div>");
+    $("#scores-column").append("<p>Scores</p>");
+
+    scores = JSON.parse(localStorage.getItem("myScores"));
+    if (scores != null) {
+        for (var i = 0;i<scores.length;i++) {
+            $("main").append("<div id=\"scores-container" + i + "\" class=\"row d-flex justify-content-center\"></div>");
+            $("#scores-container" + i).append("<div id=\"scores-table" + i + "\" class=\"w-25 d-flex justify-content-center\"></div>");
+            $("#scores-table" + i).append("<div id=\"name" + i + "\" class=\"col-lg-6 d-flex justify-content-start\"></div>");
+            $("#name" + i).append("<p>" + scores[i].name + "</p>");
+            $("#scores-table" + i).append("<div id=\"score" + i + "\" class=\"col-lg-6 d-flex justify-content-end\"></div>");
+            $("#score" + i).append("<p>" + scores[i].score + "</p>");
+        }
+    }
+
+    $("main").append("<div id=\"button-container\" class=\"row d-flex justify-content-center\"></div>");
+    $("#button-container").append("<div id=\"buttons\" class=\"w-25 d-flex justify-content-center\"></div>");
+    $("#buttons").append("<button id=\"clear-button\" class=\"btn m-1\">Clear Scores</button>");
+    $("#buttons").append("<button id=\"return-button\" class=\"btn m-1\">Return</button>");
+}
+
 $("#high-scores").on("click", function() {
-    console.log("High Scores");
+    showScores();
 });
 
-$("#start-quiz").on("click", function() {
+$("main").on("click", "#start-quiz", function() {
     Quiz();
 });
 
@@ -95,6 +130,9 @@ $("main").on("click",".answer-button",function () {
 })
 
 $("main").on("click", "#submit-button", function() {
+    if ($("#username").val() == "") {
+        return;
+    }
     var playerObj = {
         name: $("#username").val(),
         score: score
@@ -121,6 +159,20 @@ $("main").on("click", "#submit-button", function() {
         highScores = [playerObj];
         localStorage.setItem("myScores",JSON.stringify(highScores));
     }
+    showScores();
+});
+
+$("main").on("click", "#clear-button", function() {
+    localStorage.removeItem("myScores");
+    $("main").empty();
+    $("main").append("<h2 id=\"header-row\" class=\"row col-lg-12 d-flex justify-content-center\">Coding Quiz Challenge</h2><p id=\"content-row\" class=\"row col-lg-12 d-flex justify-content-center\"></p><div id=\"button-container\" class=\"row d-flex justify-content-center\"><button id=\"start-quiz\" class=\"btn\">Start Quiz</button></div>");
+    $("#content-row").text(openingText);
+});
+
+$("main").on("click", "#return-button", function() {
+    $("main").empty();
+    $("main").append("<h2 id=\"header-row\" class=\"row col-lg-12 d-flex justify-content-center\">Coding Quiz Challenge</h2><p id=\"content-row\" class=\"row col-lg-12 d-flex justify-content-center\"></p><div id=\"button-container\" class=\"row d-flex justify-content-center\"><button id=\"start-quiz\" class=\"btn\">Start Quiz</button></div>");
+    $("#content-row").text(openingText);
 });
 
 $("#content-row").text(openingText);
